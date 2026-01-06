3d958f7aa5ca021d2fd266d6418f2fab86df8df6
David Konsumer <konsumer@jetboystudio.com>
2026-01-06 13:01:52 -0800

    starting work on native host

--- README.md
+++ README.md
@@ -1,34 +1,36 @@
 The idea here is a webassembly-host implementation of [raylib](https://www.raylib.com/) 5.5 for native & web.
 
-The entire API is exposed to the webassembly, so anyone can run your game on web or without a web-browser (native wasm-host) without recompile.
+The entire API is exposed to the webassembly, so anyone can run your standalone game on web or without a web-browser (native wasm-host) without recompiling it.
 
 ## differences from raylib
 
 - export `CartInit`/`CartUpdate` for your code, instead of `main` using window/draw-management stuff (`SetTargetFPS`, `WindowShouldClose`, `BeginDrawing`, `EndDrawing`, `CloseWindow`, etc.) I might drop `CartInit` (and use main) but it's kinda nice to seperate them.
 - I have not implmented every raylib function, but I am working on adding most things to the code-generators.
+- I will probably skip file/audio callbacks
+- memory-management is generally done cart-side.
 
 ## carts
 
-You make your game as a "cart" which is just a zip file with `main.wasm` and any assets you want access to.
+You make your game as a "cart" which is just a zip-file with `main.wasm` and any assets you want access to.
 
 Technically, you can write your cart in any language that compiles to (or can be interpretred in) webassembly, but practically, it helps to have a header to make it more ergonomic.
 
 Here are the cart-languages we directly support, now:
 
 - C - it's very similar to regular raylib
-- Javascript - Uses quickjs main.wasm, and put your game in main.js (in cart) not as efficient as walt/assemblyscript, so choose those, if you can
-
-Both have very limited functions exposed (just enough for hello-world.)
+- Javascript - Uses quickjs main.wasm, and put your game in main.js (in cart.) This is not as efficient (slower, bigger file) as walt/assemblyscript, so choose those, if you can.
 
 I am also working on these, but they are not as complete:
 
-- Javascript (web) - this is similar to [raylib-wasm](https://github.com/konsumer/raylib-wasm), but you can also mount carts for filesystem, and dynamically code your game in JS, using the browser's engine, not QuickJS.
+- Javascript (web) - this is similar to [raylib-wasm](https://github.com/konsumer/raylib-wasm), but you can also mount cart for filesystem, and dynamically code your game in JS, using the browser's engine, not QuickJS. This uses the main.js in the cart, and you can skip the QuickJS main.wasm, if you don't need your game to work on native. The syntax should be mostly identical to QuickJS carts.
 - [Nelua](https://nelua.io/) - Use a lua-like language to make a compiled cart
 - [Assemblyscript](https://www.assemblyscript.org/) - Use a language very similar to typescript/javascript to make a compiled cart
 - [Walt](https://github.com/ballercat/walt) - Use a language very similar to javascript to make a light compiled cart
 - Rust
 
-## usage
+## development
+
+You will need a C compiler (clang, etc) cmake, emscripten (for web host) and carts have verious dependencies (C carts need wasi-sdk, nelua, rust, etc.) I will try to get this all building in CI, so you can find it in [releases](https://github.com/notnullgames/raycart/releases), and not need to build anything yourself.
 
 ```sh
 # build carts & web runtime, and start a local reloading web-server
@@ -55,7 +57,7 @@ npm run build:web
 # format your JS code
 npm run format
 
-# generate all the source-files I generate from the raylib JSON
+# generate all the source-files from the raylib JSON
 # be careful if you have modified anything
 # this is not really needed for most people
 npm run codegen
@@ -78,6 +80,7 @@ You can also use the native runtime (without a browser) like this:
 - host that works on ESP32 and other very low-end devices (2D-only, etc)
 - live-reloading web-build
 - live-reloading native build
+- Don't manage `BeginDrawing`/`EndDrawing` (require user to call these) so you can do other stuff in your loop (textures, etc)
 
 ## thanks
 
--- host/host_native.c
+++ host/host_native.c
@@ -1,12 +1,14 @@
 #ifndef EMSCRIPTEN
 
-#include <stddef.h>
 #include "raylib.h"
-#include "wasm_export.h"
 #include "wamr_wasi_physfs.h"
+#include "wasm_export.h"
+#include <stddef.h>
 
+// TODO: make sure these mem-sizes match web-host, so you have a known amount of RAM
 static uint32_t stack_size = 1024 * 1024 * 10; // 10 MB
 static uint32_t heap_size = 1024 * 1024 * 10;  // 10 MB
+
 static wasm_module_t module = NULL;
 static wasm_module_inst_t module_inst = NULL;
 static wasm_exec_env_t exec_env = NULL;
@@ -15,24 +17,152 @@ static wasm_function_inst_t cart_callback_init = NULL;
 static wasm_function_inst_t cart_callback_update = NULL;
 static wasm_function_inst_t cart_callback_close = NULL;
 
-bool CartInit(char *wasmBuffer, int bytesRead) {
-    // TODO: add stuff from https://github.com/notnullgames/null0/blob/main/host/src/cart_wamr.c
-    InitWindow(800, 450, "raylib [core] example - basic window");
-    SetTargetFPS(60);
-    return true;
+static void raycart_InitWindow(wasm_exec_env_t exec_env, int width, int height, char* title) {
+    InitWindow(width, height, title);
+}
+
+static void raycart_ClearBackground(wasm_exec_env_t exec_env, Color* color) {
+    ClearBackground(*color);
+}
+
+static void raycart_DrawText(wasm_exec_env_t exec_env, char * text, int posX, int posY, int fontSize, Color* color) {
+    DrawText(text, posX, posY, fontSize, *color);
+}
+
+#define raycart_native_symbols_count 3
+static NativeSymbol raycart_native_symbols[raycart_native_symbols_count] = {
+    {"InitWindow", raycart_InitWindow, "(ii$)"},
+    {"ClearBackground", raycart_ClearBackground, "(*)"},
+    {"DrawText", raycart_DrawText, "($iii*)"},
+};
+
+bool CartInit(char *wasmBytes, int wasmSize) {
+  char error_buf[128];
+  RuntimeInitArgs init_args = {0};
+
+  // TODO: both wasm hosts should probably set this, but web does not, so I will just ignore it
+  // char *argv[] = {"raycart", "CARTNAME"};
+  // wasi_set_args(2, argv);
+
+  void *heap_buf = malloc(16 * 1024 * 1024);
+  if (!heap_buf) {
+    TraceLog(LOG_FATAL, "Failed to allocate heap buffer");
+    return false;
+  }
+
+  init_args.mem_alloc_type = Alloc_With_Pool;
+  init_args.mem_alloc_option.pool.heap_buf = heap_buf;
+  init_args.mem_alloc_option.pool.heap_size = 16 * 1024 * 1024;
+  init_args.max_thread_num = 1;
+
+  if (!wasm_runtime_full_init(&init_args)) {
+    TraceLog(LOG_FATAL, "init: runtime");
+    free(heap_buf);
+    return false;
+  }
+
+  if (raycart_native_symbols_count) {
+    if (!wasm_runtime_register_natives("raycart", raycart_native_symbols, raycart_native_symbols_count)) {
+      TraceLog(LOG_FATAL, "raycart: register");
+      return false;
+    }
+  } else {
+    TraceLog(LOG_WARNING, "raycart: no symbols");
+  }
+
+  int wasi_count = sizeof(wasi_native_symbols) / sizeof(NativeSymbol);
+  if (wasi_count) {
+    if (!wasm_runtime_register_natives("wasi_snapshot_preview1", wasi_native_symbols, wasi_count)) {
+      TraceLog(LOG_FATAL, "wasi: register");
+      return false;
+    }
+    // printf("Added %d WASI functions\n", wasi_count);
+  } else {
+    TraceLog(LOG_WARNING, "wasi: no symbols");
+  }
+
+  // Load WASM module
+  module = wasm_runtime_load(wasmBytes, wasmSize, error_buf, sizeof(error_buf));
+  if (!module) {
+    TraceLog(LOG_FATAL, error_buf);
+    wasm_runtime_destroy();
+    return false;
+  }
+
+  // Instantiate the module
+  module_inst = wasm_runtime_instantiate(module, stack_size, heap_size, error_buf, sizeof(error_buf));
+  if (!module_inst) {
+    TraceLog(LOG_FATAL, error_buf);
+    wasm_runtime_unload(module);
+    wasm_runtime_destroy();
+    return false;
+  }
+
+  // Create execution environment
+  exec_env = wasm_runtime_create_exec_env(module_inst, stack_size);
+  if (!exec_env) {
+    TraceLog(LOG_FATAL, wasm_runtime_get_exception(module_inst));
+    wasm_runtime_deinstantiate(module_inst);
+    wasm_runtime_unload(module);
+    wasm_runtime_destroy();
+    return false;
+  }
+
+  cart_callback_update = wasm_runtime_lookup_function(module_inst, "CartUpdate");
+  cart_callback_close = wasm_runtime_lookup_function(module_inst, "CartClose");
+
+  // Look for main function
+  wasm_function_inst_t start_func = wasm_runtime_lookup_function(module_inst, "_start");
+  if (start_func) {
+    if (!wasm_runtime_call_wasm(exec_env, start_func, 0, NULL)) {
+      // not fatal, but warn about it
+      TraceLog(LOG_WARNING, wasm_runtime_get_exception(module_inst));
+    }
+  }
+
+  wasm_function_inst_t main_func = wasm_runtime_lookup_function(module_inst, "main");
+  if (main_func) {
+    if (!wasm_runtime_call_wasm(exec_env, main_func, 0, NULL)) {
+      // not fatal, but warn about it
+      TraceLog(LOG_WARNING, wasm_runtime_get_exception(module_inst));
+    }
+  }
+
+  wasm_function_inst_t cart_callback_load = wasm_runtime_lookup_function(module_inst, "CartInit");
+  if (cart_callback_load != NULL) {
+    if (!wasm_runtime_call_wasm(exec_env, cart_callback_load, 0, NULL)) {
+      // not fatal, but warn about it
+      TraceLog(LOG_WARNING, wasm_runtime_get_exception(module_inst));
+    }
+  }
+
+  TraceLog(LOG_INFO, "Native wasm-host loaded.");
+
+  return true;
 }
 
 bool CartUpdate() {
-    // TODO: call cart_callback_update()
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("Congrats! You created your first native window!", 190, 200, 20, LIGHTGRAY);
-    EndDrawing();
-    return true;
+  BeginDrawing();
+
+  if (cart_callback_update != NULL) {
+    if (!wasm_runtime_call_wasm(exec_env, cart_callback_update, 0, NULL)) {
+      // not fatal, but warn about it
+      TraceLog(LOG_WARNING, wasm_runtime_get_exception(module_inst));
+    }
+  }
+
+  EndDrawing();
+  return true;
 }
 
 void CartClose() {
-    // TODO: call cart_callback_close()
+ if (cart_callback_close != NULL) {
+    if (!wasm_runtime_call_wasm(exec_env, cart_callback_close, 0, NULL)) {
+      // not fatal, but warn about it
+      TraceLog(LOG_WARNING, wasm_runtime_get_exception(module_inst));
+    }
+  }
+  // TODO: do I need to cleanup any WAMR stuff?
 }
 
 #endif
