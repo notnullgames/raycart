0de888a2f78881c3e7a5fb56888ac212fa085a4a
David Konsumer <konsumer@jetboystudio.com>
2026-05-10 18:20:59 -0700

    remove confusing main.js

--- host/web/main.js
+++
@@ -1,4 +0,0 @@
-import loadRaycart from './index.js'
-
-const cartName = new URLSearchParams(location.search).get('cart') ?? 'basic.wasm'
-loadRaycart(new URL(`./carts/${cartName}`, import.meta.url))
